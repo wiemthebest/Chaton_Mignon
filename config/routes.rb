75d7ef60649891cfae05502b170fa37f7f4ed1f6
Wiem Trabelsi <wiem2007@gmail.com>
2021-06-08 14:17:33 +0200

    creation page index produits

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'items/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root 'items#index'
-  resources :items
+  resources :items, only: [:index]
   resources :carts, only: [:create, :update, :destroy, :show]
   resources :orders, only: [:new, :create, :destroy, :show]
   get 'static_pages/contact'
