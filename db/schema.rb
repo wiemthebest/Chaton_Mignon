c88f70d98209e09b921a8af79843f7b45111500a
hareish <hareish3@gmail.com>
2021-06-09 21:27:25 +0200

    frontend update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_06_09_150940) do
 
   # These are extensions that must be enabled in order to support this database
@@ -40,7 +39,6 @@ ActiveRecord::Schema.define(version: 2021_06_09_150940) do
   create_table "cart_items", force: :cascade do |t|
     t.bigint "cart_id"
     t.bigint "item_id"
-    t.integer "quantity", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cart_id"], name: "index_cart_items_on_cart_id"
@@ -94,5 +92,3 @@ ActiveRecord::Schema.define(version: 2021_06_09_150940) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
-
-
